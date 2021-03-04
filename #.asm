947a4bcbe6ad32c94215ec14811d7b633ba24662
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-03-04 00:27:36 +0000

    Add files via upload

--- #.asm
+++ #.asm
@@ -6,7 +6,20 @@ mov ds,ax
 mov ah,9
 mov dx,label
 int 240
-mov ah,1
+mov ax,cs
+mov ds,ax
+mov ah,0ah
+mov dx,labeli
+int 240
+mov ax,cs
+mov ds,ax
+mov ah,9
+mov dx,labelii
+int 240
+mov ax,cs
+mov ds,ax
+mov ah,9
+mov dx,labeliii
 int 240
 jmp loop1
 call halts
@@ -38,11 +51,77 @@ int 16h
 mov dl,al
 mov ah,2
 int 240
-jmp irets
+push cx
+jmp irets2
 inputs:
 mov ah,1
 int 240
 jmp irets
+kinput:
+mov si,dx
+ds
+mov cl,[si]
+inc si
+inc si
+mov ch,0
+mov di,si
+kinput2:
+mov ah,0
+int 16h
+cmp al,8
+jnz kinput3
+cmp ch,0
+jz kinput2
+dec ch
+dec di
+push cx
+mov ah,3
+mov bh,0
+int 10h
+cmp dl,0
+jnz kinput4
+mov dl,79
+dec dh
+kinput4:
+dec dl
+mov bh,0
+mov ah,2
+int 10h
+mov ah,0ah
+mov al,32
+mov cx,2
+mov bl,7
+mov bh,0
+int 10h
+pop cx
+jmp kinput2
+kinput3:
+cmp al,13
+jz kinput8
+cmp cl,ch
+jz kinput2
+ds
+mov [di],al
+inc di
+inc ch
+mov dl,al
+mov ah,2
+int 240
+jmp kinput2
+kinput8:
+dec si
+ds
+mov [si],ch
+jmp irets
+gver:
+mov al,5
+mov ah,0
+mov bh,0
+mov cx,0
+pop dx
+pop dx
+pop dx
+jmp irets10
 halts:
 jmp halts
 area:
@@ -73,19 +152,18 @@ mov di,132
 	add di,2
 mov ax,cs
 mov ds,ax
-mov ax,cs
-mov ds,ax
 ret
 vectorsi:
+push bp
 push si
 push di
 push ds
 push es
-push ax
+push dx
 push bx
 push cx
-push dx
-push bp
+push ax
+crts0:
 crts1:
 cmp ah,1
 jnz crts2
@@ -97,18 +175,34 @@ jmp printchr
 crts3:
 crts9:
 cmp ah,9
-jnz crts3
+jnz crts10
 jmp print
 crts10:
+cmp ah,0ah
+jnz crts11
+jmp kinput
+crts11:
+crts30h:
+cmp ah,030h
+jnz crts31h
+jmp gver
+crts31h:
+
 irets:
-pop bp
-pop dx
+pop ax
+irets2:
 pop cx
 pop bx
-pop ax
+irets10:
+pop dx
 pop es
 pop ds
 pop di
 pop si
+pop bp
 iret
-label db 13,10,'kernel version 0.01v',13,10,'$' ,0
+label db 13,10,'kernel version 0.02v',13,10,'$' ,0
+labelii db 13,10,"$",0
+labeli db 9,0
+labeliii db '          ',13,10,'$' ,0
+labeliiii db "  $",0
